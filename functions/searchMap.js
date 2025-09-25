a3f1fe1fed9ffd9e9d42614cddc4c7e75bd742c1
DomenNoTorre <mindbound11@gmail.com>
2025-09-25 21:19:14 +0200

    Added nation page, altipiani page and Reve page

---
+++ functions/searchMap.js
@@ -0,0 +1,69 @@
+const searchMap = 
+{
+  // Altipiani characters
+  "camilla": "/nationPages/altipianiPages/castPages/camilla.html",
+  "cario": "/nationPages/altipianiPages/castPages/cario.html",
+  "cosmo": "/nationPages/altipianiPages/castPages/cosmo.html",
+  "nicolas": "/nationPages/altipianiPages/castPages/nicolas.html",
+  "reve": "/nationPages/altipianiPages/castPages/reve.html",
+  "rocco": "/nationPages/altipianiPages/castPages/rocco.html",
+  "salvo": "/nationPages/altipianiPages/castPages/salvo.html",
+  "valerio": "/nationPages/altipianiPages/castPages/valerio.html",
+  "viviana": "/nationPages/altipianiPages/castPages/viviana.html",
+  
+
+
+  // 'Ard altanawue characters
+
+  // Celerración characters
+
+  // Chuma baidisha characters
+
+  // Farostia characters
+
+  // Granskog characters
+
+  // Jäädytetyt riimut characters
+  "ase": "/nationPages/riimutPages/castPages/ase.html",
+
+  // Leidrübreich characters
+
+  // Midprot characters
+
+  // Nesgen characters
+
+  // Shokuminchi kokka characters
+
+  // Sothleigh characters
+
+  // Terpayâtre characters
+
+  // The wasteland Characters
+
+  // Vriesland characters
+  "julia": "/nationPages/vrieslandPages/castPages/julia.html",
+
+  // Zloyvor characters
+  "anastasia": "/nationPages/riimutPages/castPages/anastasia.html",
+  "irina": "/nationPages/riimutPages/castPages/irina.html",  
+  "mnemos": "/nationPages/riimutPages/castPages/mnemos.html",
+
+
+  // Nations
+  "altipiani": "/nationPages/altipianiPages/altipianiMainpage.html",
+  "altanawue": "/nationPages/altanawuePages/altanawueMainpage.html",
+  "celerracion": "/nationPages/celerracionPages/celerracionMainpage.html",
+  "chuma": "/nationPages/chumaPages/chumaMainpage.html",
+  "farostia": "/nationPages/farostiaPages/farostiaMainpage.html",
+  "granskog": "/nationPages/granskogPages/granskogMainpage.html",
+  "riimut": "/nationPages/riimutPages/riimutMainpage.html",
+  "leidrubreich": "/nationPages/leidrubreichPages/leidrubreichMainpage.html",
+  "midprot": "/nationPages/midprotPages/midprotMainpage.html",
+  "nesgen": "/nationPages/nesgenPages/nesgenMainpage.html",
+  "kokka": "/nationPages/kokkaPages/kokkaMainpage.html",
+  "sothleigh": "/nationPages/sothleighPages/sothleighMainpage.html",
+  "terpayatre": "/nationPages/terpayatrePages/terpayatreMainpage.html",
+  "wasteland": "/nationPages/wastelandPages/wastelandMainpage.html",
+  "vriesland": "/nationPages/vrieslandPages/vrieslandMainpage.html",
+  "zloyvor": "/nationPages/zloyvorPages/zloyvorMainpage.html"
+};
