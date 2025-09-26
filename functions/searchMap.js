f7df31a8a68303ed4831eecf565e6d8bf97cd2f1
DomenNoTorre <mindbound11@gmail.com>
2025-09-26 17:54:13 +0200

    Added search suggestions and Zloyvor page

--- functions/searchMap.js
+++ functions/searchMap.js
@@ -1,18 +1,20 @@
 const searchMap = 
 {
   // Altipiani characters
-  "camilla": "/nationPages/altipianiPages/castPages/camilla.html",
-  "cario": "/nationPages/altipianiPages/castPages/cario.html",
-  "cosmo": "/nationPages/altipianiPages/castPages/cosmo.html",
+  "camilla": "//nationPages/altipianiPages/castPages/camilla.html",
+  "cario": "//nationPages/altipianiPages/castPages/cario.html",
+  "cosmo": "//nationPages/altipianiPages/castPages/cosmo.html",
+  "dio del vuoto": "/nationPages/altipianiPages/castPages/vicenci.html",
+  "nathan": "/nationPages/altipianiPages/castPages/nathan.html",
   "nicolas": "/nationPages/altipianiPages/castPages/nicolas.html",
   "reve": "/nationPages/altipianiPages/castPages/reve.html",
   "rocco": "/nationPages/altipianiPages/castPages/rocco.html",
   "salvo": "/nationPages/altipianiPages/castPages/salvo.html",
+  "sophia": "/nationPages/altipianiPages/castPages/sophia.html",
   "valerio": "/nationPages/altipianiPages/castPages/valerio.html",
+  "vicenci": "/nationPages/altipianiPages/castPages/vicenci.html",
   "viviana": "/nationPages/altipianiPages/castPages/viviana.html",
   
-
-
   // 'Ard altanawue characters
 
   // Celerración characters
@@ -24,46 +26,62 @@ const searchMap =
   // Granskog characters
 
   // Jäädytetyt riimut characters
+  "åse": "/nationPages/riimutPages/castPages/ase.html",
   "ase": "/nationPages/riimutPages/castPages/ase.html",
+  "runemaster": "/nationPages/riimutPages/castPages/ase.html",
 
   // Leidrübreich characters
 
   // Midprot characters
 
   // Nesgen characters
+  "osvaldo": "/nationPages/nesgenPages/castPages/osvaldo.html",
 
   // Shokuminchi kokka characters
 
   // Sothleigh characters
+  "mycroft": "/nationPages/sothleighPages/castPages/mycroft.html",
 
   // Terpayâtre characters
 
   // The wasteland Characters
+  "noah": "/nationPages/wastelandPages/castPages/noah.html",
 
   // Vriesland characters
   "julia": "/nationPages/vrieslandPages/castPages/julia.html",
 
   // Zloyvor characters
-  "anastasia": "/nationPages/riimutPages/castPages/anastasia.html",
-  "irina": "/nationPages/riimutPages/castPages/irina.html",  
-  "mnemos": "/nationPages/riimutPages/castPages/mnemos.html",
+  "anastasia": "/nationPages/zloyvorPages/castPages/anastasia.html",
+  "irina": "/nationPages/zloyvorPages/castPages/irina.html",  
+  "mnemos": "/nationPages/zloyvorPages/castPages/mnemos.html",
 
 
   // Nations
   "altipiani": "/nationPages/altipianiPages/altipianiMainpage.html",
+  "ard altanawue": "/nationPages/altanawuePages/altanawueMainpage.html",
   "altanawue": "/nationPages/altanawuePages/altanawueMainpage.html",
-  "celerracion": "/nationPages/celerracionPages/celerracionMainpage.html",
-  "chuma": "/nationPages/chumaPages/chumaMainpage.html",
+  "celerración": "/nationPages/celerracionPages/celerracionMainpage.html",
+  "chuma baidisha": "/nationPages/chumaPages/chumaMainpage.html",
   "farostia": "/nationPages/farostiaPages/farostiaMainpage.html",
   "granskog": "/nationPages/granskogPages/granskogMainpage.html",
+  "jäädytetyt riimut": "/nationPages/riimutPages/riimutMainpage.html",
   "riimut": "/nationPages/riimutPages/riimutMainpage.html",
-  "leidrubreich": "/nationPages/leidrubreichPages/leidrubreichMainpage.html",
+  "leidrübreich": "/nationPages/leidrubreichPages/leidrubreichMainpage.html",
   "midprot": "/nationPages/midprotPages/midprotMainpage.html",
   "nesgen": "/nationPages/nesgenPages/nesgenMainpage.html",
+  "shokuminchi kokka": "/nationPages/kokkaPages/kokkaMainpage.html",
   "kokka": "/nationPages/kokkaPages/kokkaMainpage.html",
   "sothleigh": "/nationPages/sothleighPages/sothleighMainpage.html",
-  "terpayatre": "/nationPages/terpayatrePages/terpayatreMainpage.html",
+  "terpayâtre": "/nationPages/terpayatrePages/terpayatreMainpage.html",
+  "the wasteland": "/nationPages/wastelandPages/wastelandMainpage.html",
   "wasteland": "/nationPages/wastelandPages/wastelandMainpage.html",
   "vriesland": "/nationPages/vrieslandPages/vrieslandMainpage.html",
-  "zloyvor": "/nationPages/zloyvorPages/zloyvorMainpage.html"
+  "zloyvor": "/nationPages/zloyvorPages/zloyvorMainpage.html",
+
+  // General stuff
+  "about us": "webpages/aboutus.html",
+  "books": "index.html",
+  "home": "index.html",
+  "mainpage": "index.html",
+  "nations": "/nationPages/nations.html",
 };
